c23bc4a94b518212f765623ce2fb63e28714b519
380509011374x <380509011374x@gmail.com>
2023-02-11 19:03:01 +0300

    Add reducer,actions

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import reducer from "./redux/reducer";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+const store = createStore(reducer)
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>
 );
+//TODO добавить redux
+//TODO сделать компонент календаря
+//TODO добавить логику календаря
+//TODO переделать удаление на айди
+//TODO сделать формы для добавления/редактирования событий
+//TODO сделать страницу списка событий на день
+//TODO сделать страницу для календаря
+//TODO добавить роутинг
+//TODO добавить уведомления
+
 
---
+++ src/redux/reducer.typing.ts
@@ -0,0 +1,25 @@
+import {Dayjs} from "dayjs";
+
+export enum EnumAction {
+    addEvent = 'add-event',
+    removeEvent = 'remove-event',
+    updateEvent = 'update-event'
+}
+
+export type EventType = {
+    id: string
+    title: string,
+    start: Dayjs,
+    end: Dayjs,
+    notification: Dayjs,
+    type: string
+}
+export type StoreType = {
+    events: Record<string, EventType[]>
+}
+
+
+export type ActionsAddEvent = {
+    type: EnumAction
+    payload: EventType
+}
