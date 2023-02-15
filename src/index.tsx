471736a5762f079b3daeba28e9591485501ee36a
380509011374x <380509011374x@gmail.com>
2023-02-15 14:41:25 +0300

    ready

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,9 @@ import { Provider } from 'react-redux';
 import { StoreType } from './redux/reducer.typing';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './pages/Main';
-
 import Calendar from './components/Calendar';
 import EventList from './components/EventList';
 import ErrorPage from './pages/ErrorPage';
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 function saveToLocalStorage(state: StoreType) {
